3e8fdf76caf3781eaf6f82829393f1b5c9075cf0
nikzanda <nikzanda@hotmail.it>
2021-06-26 15:58:33 +0200

    async await crea ordini

--- backend/app/controllers/order.controller.js
+++ backend/app/controllers/order.controller.js
@@ -3,13 +3,13 @@ const Order = db.orders
 // const jwt = require("jsonwebtoken")
 const { validationResult } = require("express-validator")
 
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
     const errors = validationResult(req)
 
     if (!errors.isEmpty())
         return res.status(422).send({ errors: errors.array() })
 
-    const order = Order.create({
+    const order = await Order.create({
         user_id: 1,
         article_id: req.body.articleID,
         quantity: req.body.quantity,
