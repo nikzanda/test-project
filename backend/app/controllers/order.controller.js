128eaeb66b2efe698471c3b5fc0f744b52c297eb
nikzanda <nikzanda@hotmail.it>
2021-06-26 17:32:22 +0200

    fix order controller findAll

--- backend/app/controllers/order.controller.js
+++ backend/app/controllers/order.controller.js
@@ -1,9 +1,26 @@
 const db = require("../models")
 const Order = db.orders
+const Article = db.articles
 const { validationResult } = require("express-validator")
 
 exports.findAll = async (req, res) => {
-    res.status(200).json(await Order.findAll({ where: { user_id: req.user.id } }))
+    const orders = await Order.findAll({ where: { user_id: req.user.id } })
+
+    let articles = orders.map(order => JSON.parse(order.articles).map(article => article.id))
+    articles = await Article.findAll({
+        where: {
+            id: articles.flat()
+        }
+    })
+    articles = articles.map(article => article.dataValues)
+
+    res.status(200).json(orders.map(order => ({
+        ...order.dataValues,
+        articles: JSON.parse(order.articles).map(article => ({
+            ...articles.find(thatArticle => thatArticle.id === article.id),
+            cartQuantity: article.quantity
+        }))
+    })))
 }
 
 exports.create = async (req, res) => {
