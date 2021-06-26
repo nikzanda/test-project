5295a73d11346d0cc184f6813ff5e2f35ce1f167
nikzanda <nikzanda@hotmail.it>
2021-06-26 15:23:50 +0200

    lista articoli

---
+++ frontend/src/components/ArticleCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-col lg="3" md="3" sm="4" xs="6">
+    <v-card>
+      <v-card-title>{{ article.name }}</v-card-title>
+      <v-card-subtitle>
+        Quantità disponibile: {{ article.quantity }}
+      </v-card-subtitle>
+
+      <v-card-actions>
+        <v-btn color="success" small>Aggiungi al carrello</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "ArticleCard",
+  props: {
+    article: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,61 @@
 <template>
-  <h1>login effettuato</h1>
+  <v-container fluid>
+    <h1 class="text-center">Articoli</h1>
+
+    <v-data-iterator
+      :items="articles"
+      :search="search"
+      :loading="loading"
+      :items-per-page="articles.length"
+      hide-default-footer
+    >
+      <template #header>
+        <v-text-field
+          v-model="search"
+          clearable
+          flat
+          solo-inverted
+          hide-details
+          prepend-inner-icon="mdi-magnify"
+          label="Search"
+        ></v-text-field>
+      </template>
+
+      <template #default="{ items }">
+        <v-row>
+          <ArticleCard v-for="item in items" :key="item.id" :article="item" />
+        </v-row>
+      </template>
+    </v-data-iterator>
+  </v-container>
 </template>
 
 <script>
+import ArticleCard from "../components/ArticleCard";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    ArticleCard
+  },
+  data: () => ({
+    articles: [],
+    search: "",
+    loading: false
+  }),
+  created() {
+    this.loadArticles();
+  },
+  methods: {
+    loadArticles() {
+      this.loading = true;
+
+      this.$axios("/articles/")
+        .then(({ data }) => {
+          this.articles = data;
+        })
+        .finally(() => (this.loading = false));
+    }
+  }
 };
 </script>
