26ef642d88fd5e3d328a7fb1248c87f767fde758
nikzanda <nikzanda@hotmail.it>
2021-06-26 13:38:13 +0200

    user email - password

--- backend/app/models/user.model.js
+++ backend/app/models/user.model.js
@@ -5,10 +5,21 @@ module.exports = (sequelize, Sequelize) => {
             type: Sequelize.INTEGER,
             autoIncrement: true
         },
+        email: {
+            type: Sequelize.STRING,
+            unique: true,
+            allowNull: false
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
         name: {
             type: Sequelize.STRING
         }
     })
+
+    User.create({ email: "test@mail.com", password: "1234", name: "John Doe" })
 
     return User
 }
