646813beb16053ee9b850f3a5329df27d4a6d358
nikzanda <nikzanda@hotmail.it>
2021-06-26 18:09:15 +0200

    card aggiungi prodotto al carrello

--- frontend/src/views/Add.vue
+++ frontend/src/views/Add.vue
@@ -1,7 +1,22 @@
 <template>
   <v-dialog v-model="dialog" persistent max-width="600px">
     <v-card>
-      {{ articleID }}
+      <v-card-title>Aggiungi articolo al carrello</v-card-title>
+
+      <v-card-text>
+        <v-radio-group v-model="radioGroup">
+          <v-radio label="In un nuovo ordine" :value="0"></v-radio>
+          <v-radio label="In un ordine già esistente" :value="1"></v-radio>
+        </v-radio-group>
+
+        <v-autocomplete
+          v-show="radioGroup == 1"
+          v-model="order"
+          label="Ordini aperti"
+          :items="pendingOrders"
+        ></v-autocomplete>
+      </v-card-text>
+
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn color="error" @click="cancel">Annulla</v-btn>
@@ -12,6 +27,7 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   name: "Add",
   props: {
@@ -21,8 +37,16 @@ export default {
     }
   },
   data: () => ({
-    dialog: true
+    dialog: true,
+    radioGroup: 0,
+    order: null
   }),
+  computed: {
+    ...mapGetters("orders", ["pending"]),
+    pendingOrders() {
+      return this.pending.map(order => order.id);
+    }
+  },
   methods: {
     cancel() {
       this.dialog = false;
