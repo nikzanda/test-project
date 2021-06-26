842b5e8f17760243ee20bcaaf8e66ddca6285951
nikzanda <nikzanda@hotmail.it>
2021-06-26 12:14:55 +0200

    configurazione packages, ambiente e server

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+require("dotenv").config({ path: path.join(__dirname, `${process.env.NODE_ENV || "development"}.env`) });
+
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json()); // content-type: application/json
+app.use(bodyParser.urlencoded({ extended: true })); // content-type: application/x-www-form-urlencoded
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Environment: ${process.env.NODE_ENV}`);
+  console.log(`Server running on port ${PORT}.`);
+});
