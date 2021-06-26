75e3cbe024823247cba8cb6cc39ece2b142603e5
nikzanda <nikzanda@hotmail.it>
2021-06-26 16:58:29 +0200

    tabella orders colonna articles di tipo JSON

--- backend/app/models/order.model.js
+++ backend/app/models/order.model.js
@@ -13,19 +13,22 @@ module.exports = (sequelize, Sequelize) => {
                 key: "id"
             }
         },
-        article_id: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-            references: {
-                model: "articles",
-                key: "id"
-            }
+        articles: {
+            type: Sequelize.JSON
         },
-        quantity: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-            defaultValue: 1
-        },
+        // article_id: {
+        //     type: Sequelize.INTEGER,
+        //     allowNull: false,
+        //     references: {
+        //         model: "articles",
+        //         key: "id"
+        //     }
+        // },
+        // quantity: {
+        //     type: Sequelize.INTEGER,
+        //     allowNull: false,
+        //     defaultValue: 1
+        // },
         status: {
             type: Sequelize.ENUM('pending', 'settled'),
             allowNull: false
